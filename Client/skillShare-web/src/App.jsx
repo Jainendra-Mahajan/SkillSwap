a2f038617fabd4c3cc5992d2564dbb67ce0e486c
Jainendra <jainendra.mahajan456@gmail.com>
2025-07-06 11:47:07 +0530

    initial project setup with React, Vite, Tailwind CSS, and DaisyUI

---
+++ Client/skillShare-web/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-extralight'>Hello World</h1>
+        <button className="btn">Default</button>
+      </div>
+    </>
+  )
+}
+
+export default App
